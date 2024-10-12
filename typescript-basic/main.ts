3d89ff5654b5b734bd44c69f6d335f2dfe4aea9e
ydjwls <xqhfkfpalx@naver.com>
2024-10-12 19:37:26 +0900

    study: typescript - enum

--- typescript-basic/main.ts
+++ typescript-basic/main.ts
@@ -150,3 +150,29 @@ function addManyToCart(...ids: string[]) {
         addToCart(id);
     }
 }
+
+// enum 
+enum Size {
+    S = 'S',
+    M = 'M',
+    L = 'L',
+    XL = 'XL',
+}
+
+let product2: {
+    id: string,
+    name:string,
+    price: number;
+    membersOnly?: boolean;
+    sizes: Size[];
+} = {
+    id: 'c001',
+    name: '코드잇 블랙 후디',
+    price: 129000,
+    sizes: [Size.M, Size.L], 
+}; 
+
+console.log(Size.S);   
+console.log(Size.M);   
+console.log(Size.L);   
+console.log(Size.XL);  
